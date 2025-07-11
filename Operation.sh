09650dbd1b1c84c88809d60fdc8fd9340dd28da8
Sahebrao Shivaji Rahire <Rahires007@gmail.com>
2025-07-11 23:47:34 +0530

    Create Operation.sh

---
+++ Operation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#All Arithmetic operation 
+echo "Enter 2 no..."
+read Num1
+read Num2
+echo "Add =$((Num1+Num2))"
+echo "Sub =$((Num1-Num2))"
+echo "Multi =$((Num1*Num2))"
+echo "Div =$((Num1/Num2))"
